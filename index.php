501f2437f9c30e5211f95e5561ac9e165a146d17
Carlos William Vasquez Vargas <willrp2527@gmail.com>
2021-08-26 19:22:21 -0500

    start code and end code

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'plantillas/declaracion_inicio.inc.php';
+
+?>
+<h1> Platzi</h1>
+<p>Create text y write code</p>
+<?php
+include_once 'plantillas/declaracion_cierre.inc.php';
+?>
