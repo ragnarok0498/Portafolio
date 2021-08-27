3b4fbd125b2f55d3c296da2d3eaab76e30351ab7
Carlos William Vasquez Vargas <willrp2527@gmail.com>
2021-08-27 14:17:19 -0500

    fontawesome

--- plantillas/declaracion_inicio.inc.php
+++ plantillas/declaracion_inicio.inc.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Dashboard</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
 </head>
--- plantillas/navbar.inc.php
+++ plantillas/navbar.inc.php
@@ -3,50 +3,35 @@
         <div class="col-md-12">
             <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                 <div class="container-fluid px-0">
-                    <a class="navbar-brand font-weight-bolder ms-sm-3" href=" " rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
-                        William 
-                        <span>Vasquez</span>
+                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="" rel="tooltip" data-placement="bottom" target="_blank">
+                        William Vasquez
                     </a>
                     <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
-                        <ul class="navbar-nav navbar-nav-hover mx-auto">
-                            <li class="nav-item dropdown dropdown-hover mx-2">
-                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">
-                                   Inicio
-                                    
+                        <ul class="navbar-nav navbar-nav-hover mx-auto bs-gray-800">
+                            <li class="nav-item mx-2">
+                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Inicio 
                                 </a>
                             </li>
-                            <li class="nav-item dropdown dropdown-hover mx-2">
-                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" id="dropdownMenuAccount" data-bs-toggle="dropdown" aria-expanded="false">
-                                   Sobre mi
-                                    
+                            <li class="nav-item mx-2">
+                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Sobre mi
                                 </a>
                             </li>
-                            <li class="nav-item dropdown dropdown-hover mx-2">
-                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" id="dropdownMenuBlocks" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li class="nav-item mx-2">
+                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
                                     Habilidades
-                                    
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-animation dropdown-lg dropdown-lg-responsive p-3 border-radius-lg mt-0 mt-lg-3" aria-labelledby="dropdownMenuBlocks">
-
-                                </div>
                             </li>
-                            <li class="nav-item dropdown dropdown-hover mx-2">
-                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" id="dropdownMenuDocs" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li class="nav-item mx-2">
+                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
                                     Proyectos
-                                   
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-animation dropdown-lg mt-0 mt-lg-3 p-3 border-radius-lg" aria-labelledby="dropdownMenuDocs">
-
-                                </div>
                             </li>
-                            <li class="nav-item dropdown dropdown-hover mx-2">
-                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" id="dropdownMenuDocs" data-bs-toggle="dropdown" aria-expanded="false">
-                                   Contáctame
-                                   
+                            <li class="nav-item  mx-2">
+                                <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Contáctame
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-animation dropdown-lg mt-0 mt-lg-3 p-3 border-radius-lg" aria-labelledby="dropdownMenuDocs">
-
-                                </div>
                             </li>
                         </ul>
                     </div>
