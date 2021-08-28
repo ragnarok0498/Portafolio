0de6a01e73221a46e7ec2e8814d1e2b46947a9f0
Carlos William Vasquez Vargas <willrp2527@gmail.com>
2021-08-27 20:39:33 -0500

    particulas

--- plantillas/declaracion_inicio.inc.php
+++ plantillas/declaracion_inicio.inc.php
@@ -8,7 +8,10 @@
     <title>Dashboard</title>
     <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
 
 </head>
 
 <body>
+
+    <div id="particles-js"></div>
