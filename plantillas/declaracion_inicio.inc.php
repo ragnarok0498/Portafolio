33d44130eb2e63a572deee533046cb26121aa4e3
Carlos William Vasquez Vargas <willrp2527@gmail.com>
2021-09-01 23:26:01 -0500

    Cambios css

--- plantillas/declaracion_inicio.inc.php
+++ plantillas/declaracion_inicio.inc.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Dashboard</title>
     <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="css/estilos.css" rel="stylesheet">
 
